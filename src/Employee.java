26a74524df12d5e61f93d3baf108fae5d594fcb1
askar <tashtandinov.oskar@yandex.ru>
2022-10-11 14:34:32 +0700

    Коммит по курсачу исправленный

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,54 @@
 public class Employee {
-    public static void ddd(){
-        System.out.println("dfff");
+    private final String name;
+    private final String surname;
+    private final String middleName;
+    private int salary;
+    private int department;
+    private static int counter = 0;
+    private int id = counter++;
+
+    public Employee(String name, String surname, String middleName, int department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.middleName = middleName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getMiddleName() {
+        return this.middleName;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getSalary() {
+        return this.salary = salary;
+    }
+
+    public int setSalary(int salary) {
+        return this.salary = salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+    @Override
+    public String toString() {
+        return "Сотрудник: " + getName() + " " + getMiddleName() + " " + getSurname() + ", " + getDepartment() + " Отдел , " + getSalary() + getId();
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,64 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("dfff");
+    private static final Employee[] Employee = new Employee[10];
+
+    public static void main(String[] args) {
+        Employee[0] = new Employee("Владимир", "Владимиров", "Владимирович", 1, 35000);
+        Employee[1] = new Employee("Владислав", "Владиславов", "Владиславович", 2, 15000);
+        Employee[2] = new Employee("Иван", "Харченин", "Владиславович", 3, 15000);
+        Employee[3] = new Employee("Гоша", "Иванин", "Харитонович", 4, 25000);
+        Employee[4] = new Employee("Тихон", "Елахин", "Владиславович", 5, 35000);
+        Employee[5] = new Employee("Алан", "Елесов", "Валидович", 1, 45000);
+        Employee[6] = new Employee("Банан", "Мангов", "Попович", 2, 25000);
+        Employee[7] = new Employee("Кокос", "Пеструшкин", "Араратович", 3, 15000);
+        Employee[8] = new Employee("Афанасий", "Фет", "Алексеевич", 4, 55000);
+        Employee[9] = new Employee("Иван", "Бунин", "Ибрагимович", 5, 65000);
+        for (int i = 0; i < Employee.length; i++) {
+            System.out.println(Employee[i]);
+
+        }
+        System.out.println("Сумма зарплат всех сотрудников: " + sumSalary());
+        System.out.println("Максимальная зарплата: " + maxSalaryEmployee());
+        System.out.println("Минимальная зарплата: " + minSalaryEmployee());
+        System.out.println("Минимальная зарплата: " + averageValue());
+
     }
+
+    private static int sumSalary() {
+        int sum = 0;
+        for (int i = 0; i < Employee.length; i++) {
+            if (Employee[i] != null) {
+                sum += Employee[i].getSalary();
+            }
+        }
+        return sum;
+    }
+    private static Employee maxSalaryEmployee () {
+        int max = Integer.MIN_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : Employee) {
+            if (employee != null && employee.getSalary() > max) {
+                max = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
+    }
+    private static Employee minSalaryEmployee () {
+        int min = Integer.MAX_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : Employee) {
+            if (employee != null && employee.getSalary() < min) {
+                min = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
+    }
+    private static double averageValue(){
+        double average = sumSalary() / 10;
+        return  average;
+    }
+
 }
 
 
